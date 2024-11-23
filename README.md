5382e7a481d47d5bbbd132b78ae61458c3cd126d
Sindre Savjord Win <ss@spacetrail.cc>
2024-11-23 15:57:23 +0100

    todo multicore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # primtall
 Fortran finn primtall
+# Compile
+gfortran prime.f90 -o prime
+
+# Run
+./prime
+
+# Todo
+Multicore
--- prime.f90
+++ prime.f90
@@ -10,24 +10,18 @@ program primtall_kalkulator
     allocate(array_prim(0));
     
     print *, "Hvor mange primtall vil du ha: "
-    read *, antall_primtall
-    !antall_primtall = 10
+    read *, antall_primtall ! Lese antall primtall
 
-    sum_up = 2
+    sum_up = 2 ! Starter på 2 og jobber oppover.
     do
         sum_down = sum_up
-        !print *, "Første loop"
         primtall = .true.
         do
-            !print *, "Andre loop"
-            !print *, "sum_up: ", sum_up, "sum_down", sum_down 
+            ! Vi kom oss ned til 1, kan ikke dele mer.
             if(sum_down < 2) then
-                !print *, "Er det primtall?: ", sum_up, primtall
+                ! Fant et primtall
                 if(primtall) then
                     antall_elementer = antall_elementer + 1
-                    !verdi = antall_elementer
-                    !print *, sum_down
-                    !print *, "Legger til sum_down:", sum_up
                     print *, "Fant primtall: ", sum_up
                     call legg_til_array(array_prim, sum_up)
                     exit
@@ -43,29 +37,26 @@ program primtall_kalkulator
             if(rest == 0.0) then
                 primtall = .false.
                 exit
+            ! vet ikke, funker uten
             !else 
             !    primtall = .true.
             end if
-            sum_down = sum_down - 1
+            sum_down = sum_down - 1 ! Tell nedover, 10/10, 10/9, 10/8 osv.
         end do
-        !sum_up = sum_up + 1
         if(size(array_prim) >= antall_primtall) then 
             print *, "Traff exit,", size(array_prim), "Antall prim: ", antall_primtall
             exit
         else 
-            sum_up = sum_up + 1
+            sum_up = sum_up + 1 ! Tell oppover og finn neste primtall.
         end if
     end do
+! Ferdig å kalkulere primtall.
 print *, "Ferdig"
 
-print *, size(array_prim)
-
-
+!print *, "Antall primtall funnet: ", size(array_prim) 
 do i = 1, size(array_prim)
     print *, "Primtall: ", i, ":", array_prim(i)
 end do
-
-!print *, array_prim(2)
 
 contains
 
